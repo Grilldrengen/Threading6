3adfc4f170718e2be8a026c797e657cfd322262c
Nikolaj Grill Nielsen <nikolaj@grillnielsen.dk>
2017-02-01 08:53:25 +0100

    Threads
    Thread

---
+++ Threading6/Printing.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Threading6
+{
+    public class Printing
+    {
+        
+
+        private int sum = 0;
+        private int amount = 60;
+        public void asterisks()
+        {
+            
+            for (int i = 0; i < 60; i++)
+            {
+                Console.Write("*");  
+            }
+            math();
+            Console.WriteLine();
+            
+
+        }
+
+        public void gardenGates()
+        {
+            
+            for (int i = 0; i < 60; i++)
+            {
+                Console.Write("#");
+            }
+            math();
+            Console.WriteLine();
+            
+        }
+
+        public void math()
+        {
+            sum += amount;
+            Console.Write(sum);
+        }
+    }
+}
---
+++ Threading6/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Threading6
+{
+    public class Program
+    {
+        static Printing p;
+
+        static object locker = new object();
+        static void ThreadMain1()
+        {
+            while (true)
+            {
+                Monitor.Enter(locker);
+                p.asterisks();
+                Thread.Sleep(1000);
+                Monitor.Pulse(locker);
+                Monitor.Wait(locker);
+            }
+        }
+
+        static void ThreadMain2()
+        {
+            while (true)
+            {
+                Monitor.Enter(locker);
+                p.gardenGates();
+                Thread.Sleep(1000);
+                Monitor.Pulse(locker);
+                Monitor.Wait(locker);
+            }
+        }
+        static void Main(string[] args)
+        {
+            p = new Printing();
+
+            Thread tid1 = new Thread(new ThreadStart(ThreadMain1));
+            Thread tid2 = new Thread(new ThreadStart(ThreadMain2));
+
+            tid1.Start();
+            tid2.Start();
+        }
+    }
+}
